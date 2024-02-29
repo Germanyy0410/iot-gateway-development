ff71a851d3f3d7da5f2aa98400327b3dcc05b06e
Germanyy0410 <tienduchoang136@gmail.com>
2024-02-29 20:31:41 +0700

    feat: create simple_ai and uart

---
+++ python/uart.py
@@ -0,0 +1,46 @@
+import serial.tools.list_ports
+
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    comPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            comPort = (splitPort[0])
+    return "COM10"
+
+
+if getPort() != "None":
+    ser = serial.Serial(port=getPort(), baudrate=115200)
+
+mess = ""
+def processData(client, data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+    if splitData[1] == "T":
+        client.publish("sensor1", splitData[2])
+    elif splitData[1] == "H":
+        client.publish("sensor2", splitData[2])
+
+def readSerial(client):
+    bytesToRead = ser.inWaiting()
+    if bytesToRead > 0:
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(client, mess[start:end + 1])
+            if end == len(mess):
+                mess = ""
+            else:
+                mess = mess[end + 1:]
+
+def writeData(data):
+    ser.write(str(data).encode())
